c1834ca2c3dece9ee3aff8900670a36023207d66
Khanyi Gina <113812456+BuKh0sin1@users.noreply.github.com>
2026-07-30 21:24:31 +0200

    Enhance Typewriter options with styling properties
    Added color, font-family, and font-weight properties to Typewriter options.

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ new Typewriter("#myname", {
   strings: "Khanyi Gina",
   autoStart: true,
   cursor: null,
-  delay: 400
+  delay: 400, 
+  color: black, 
+  font-family: var(--heading-font-family), 
+  font-weight: bold,
 });
 
 let heading = document.querySelector("#myname");
