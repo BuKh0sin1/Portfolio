817a0b78240a9ffaa926286c9ed4db40f16a7c3d
Khanyi Gina <113812456+BuKh0sin1@users.noreply.github.com>
2026-07-30 17:31:18 +0200

    Added index.js file

---
+++ index.js
@@ -0,0 +1,9 @@
+let challenge2Element = document.querySelector("#challenge-2");
+challenge2Element.innerHTML = "Khanyi Gina";
+
+new Typewriter("#challenge-3", {
+  strings: "Khanyi Gina",
+  autoStart: true,
+  cursor: null,
+  delay: 400
+});
