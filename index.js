282af435b9fa92ea1a216043eb4741dedb02aa4f
Khanyi Gina <113812456+BuKh0sin1@users.noreply.github.com>
2026-07-30 21:47:05 +0200

    Fix font weight and add font family to heading

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ new Typewriter("#myname", {
 
 let heading = document.querySelector("#myname");
 heading.style.color = "black";
-heading.style.fontWeight = "bold",
+heading.style.fontWeight = "bold";
+heading.style.fontFamily = "Playfair Display", serif";
